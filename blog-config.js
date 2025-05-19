a8bcacebaa412cbfbbbfc6f805d1352f7eeb0849
jin-daram <jindaram1999@gmail.com>
2025-05-19 10:34:49 +0900

    fix: blog-config 수정 (타이틀 및 설명란 수정)

--- blog-config.js
+++ blog-config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  title: "개발로그",
-  description: "많은 문제들을 한번 해결해봅시다.",
+  title: "Let's solve it",
+  description: "문제들을 한번 해결해봅시다.",
   author: "jin-daram",
   siteUrl: "https://jin-daram.github.io/",
   links: {
     github: "https://github.com/jin-daram",
   },
   useAbout: false,
-  // See https://giscus.app/
   giscus: {
-    repo: "jin-daram/jin-daram.github.io",
+    repo: "jin-daram/jin-daram.github.io",  
     repoId: "R_kgDOOqRNOw",
     category: "Announcements",
     categoryId: "DIC_kwDOOqRNO84CqKzM",
